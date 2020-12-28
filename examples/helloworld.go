d39b14777907a8e9cc85bf2af531946cb59ed943
Maros Kukan <maros.kukan@me.com>
2020-12-28 13:29:10 +0100

    Cleanup.

--- examples/helloworld.go
+++ examples/helloworld.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+func num() {
+	var num1 = 5
+	var rates float32 = 4.5
+	fmt.Println("num1 is " +
+	strconv.Itoa(num1) +
+	" and rates is " + 
+	strconv.FormatFloat(
+	float64(rates), 'f',2,32))
+}
 
 func main() {
 	fmt.Println("Hello world")
+	// Calling num function
+	num()
 }
