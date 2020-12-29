9f8aa69fea6b605818aa375160c62efce528d68a
Maros Kukan <maros.kukan@me.com>
2020-12-29 19:41:10 +0100

    Simplified

--- examples/helloworld.go
+++ examples/helloworld.go
@@ -2,21 +2,8 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
-func num() {
-	var num1 = 5
-	var rates float32 = 4.5
-	fmt.Println("num1 is " +
-	strconv.Itoa(num1) +
-	" and rates is " + 
-	strconv.FormatFloat(
-	float64(rates), 'f',2,32))
-}
-
 func main() {
-	fmt.Println("Hello world")
-	// Calling num function
-	num()
+	fmt.Println("Hello World")
 }
