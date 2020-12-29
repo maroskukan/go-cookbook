1b3ea156462a526bce488fe2970ca9eb888430a3
Maros Kukan <maros.kukan@me.com>
2020-12-29 19:55:23 +0100

    Comments examples addded

--- examples/helloworld.go
+++ examples/helloworld.go
@@ -1,8 +1,15 @@
 package main
 
+// This is an import block, which allows multiple packages to be imported without
+// repeating the "import" keyword
 import (
 	"fmt"
 )
+
+/*
+The main function, when part of the main package,
+identifies the entry point of an application.
+*/
 
 func main() {
 	fmt.Println("Hello World")
